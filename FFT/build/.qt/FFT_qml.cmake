68c39f34256776980d6731a5d77d84144f9f4ce3
russkiylis <russkiylis@koshy.ru>
2026-07-29 21:21:32 +0300

    Убрал папку build и поправил .gitignore

--- FFT/build/.qt/FFT_qml.cmake
+++
@@ -1,9 +0,0 @@
-
-set(target "FFT")
-set(working_dir "C:/Users/royal/source/repos/FFT/FFT")
-set(src_and_dest_list
-    "C:/Users/royal/source/repos/FFT/FFT/src/ui/qml/Main.qml"
-    "C:/Users/royal/source/repos/FFT/FFT/build/FFTApp/src/ui/qml/Main.qml"
-
-)
-set(timestamp_file "C:/Users/royal/source/repos/FFT/FFT/build/.qt/FFT_qml.txt")
--- FFT/build/CMakeFiles/FFT.dir/build/.qt/rcc/qrc_FFT_raw_qml_0.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/FFT.dir/build/.qt/rcc/qrc_FFT_raw_qml_0.cpp.obj: \
- C:\Users\royal\source\repos\FFT\FFT\build\.qt\rcc\qrc_FFT_raw_qml_0.cpp
